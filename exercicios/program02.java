d7200151676401bb124ef8c2633700a40e01261e
LuKKas <lucasmoraes3306@gmail.com>
2026-06-07 13:42:00 -0300

    Add files via upload

---
+++ exercicios/program02.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class program02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número de 1 a 12: ");
+        int numeroDoMes = sc.nextInt();
+
+        String mes;
+
+        switch (numeroDoMes) {
+            case 1:
+                mes = "janeiro";
+                break;
+            case 2:
+                mes = "fevereiro";
+                break;
+            case 3:
+                mes = "março";
+                break;
+            case 4:
+                mes = "abril";
+                break;
+            case 5:
+                mes = "maio";
+                break;
+            case 6:
+                mes = "junho";
+                break;
+            case 7:
+                mes = "julho";
+                break;
+            case 8:
+                mes = "agosto";
+                break;
+            case 9:
+                mes = "setembro";
+                break;
+            case 10:
+                mes = "outubro";
+                break;
+            case 11:
+                mes = "novembro";
+                break;
+            case 12:
+                mes = "dezembro";
+                break;
+            default:
+                mes = "Mês inválido!";
+        }
+        System.out.println("Dia da semana: " + mes);
+        sc.close();
+    }
+}
